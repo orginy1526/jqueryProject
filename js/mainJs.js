1559d38558f7cd9df6549bf53d5fd4728d815d2c
Or <orginy1526@gmail.com>
2022-08-18 04:33:20 +0300

    all toggle & modal functionallty

--- js/mainJs.js
+++ js/mainJs.js
@@ -6,6 +6,7 @@ $(function () {
         console.log(1);
         api();
         setInterval(() => {
+            location.reload(true);
             console.log('reload');
             api();
         }, 120000);
@@ -128,44 +129,209 @@ $(function () {
                     console.log(crypto);
                     arr.push(crypto);
                     $('#cards').empty();
-                    $('#cards').append(displayCards(arr), moreInfo(arr));
+                    $('#cards').append(displayCards(arr), moreInfo(arr), toggleSwitch(arr));
                 }
             });
         });
     }
 
+    // global variables
+    let cards = [];
+    let cenceledCards = [];
+    let lastCard = {};
+
     // toggle switch
     function toggleSwitch(response) {
         console.log(8);
-        let arr = [];
-        $.map(response, function (_event, i) {
-            let toggle = $(`#toggleSwitch${i}`);
-            toggle.html('<label class="switch"><input type="checkbox"><span class="slider round"></span></label>');
-            $(toggle).one('click', function () {
-                arr.push(this);
-                console.log(arr);
-                // arr pop last added label
-                // make sure array length is 1 
-                // then make sure array length is no more than 5
-                // when disabled - give the option to add again
-
-                // if press odd - go to array
-                // if press even - out of the array
-
-                // if more then 5 - everyone else disabled
-                if (arr.length > 1) {
-                    toggle.html('<label class="switch"><input type="checkbox" disabled><span class="slider round"></span></label>');
-                    arr.pop(this);
+        $.map(response, function (card, i) {
+            const toggle = $(`#toggleSwitch${i}`);
+            toggle.html(`<label class="switch"><input id="input${i}" type="checkbox"><span class="slider round"></span></label>`);
+            $(`#input${i}`).click(function () {
+                if ($(`#input${i}`).is(':checked')) {
+                    if (!cards.includes(card)) {
+                        cards.push(card);
+                    }
+                }
+                liveReports(cards);
+                if (cards.length > 5) {
+                    lastCard = card;
+                    cards.pop(card);
+                    modal();
                 }
             });
         });
     }
 
     // modal
-    function modal() { }
+    function modal() {
+
+        $.map(cards, function (card, i) {
+            console.log(card);
+            $("#modalBtn").trigger("click");
+
+            $("#ulModal").append(
+                `<li><img src="
+                ${card.image.small}" alt="Coin Icon"> ${card.name} <button id="btn${i}" type="button" class="del btn btn-primary">delete</button></li>`
+            );
+
+            console.log(cards);
+            cenceledCards = cards.slice(0);
+            console.log($('#cancel'));
+            $('#cancel').click(function () {
+                cencelCard(cenceledCards);
+            });
+
+            $(`#btn${i}`).click(function () {
+                deleteCard($(`#btn${i}`));
+            });
+
+        });
+    }
+
+    // delete card
+    function deleteCard(btn) {
+        const liToRemove = btn.parent()[0];
+        liToRemove.remove();
+        const indexOfObject = cards.findIndex(object => {
+            return object.name === object.name;
+        });
+
+        cards.splice(indexOfObject, 1);
+
+        $.map(cards, function (card, i) {
+            console.log(card);
+            if (!cards.includes(lastCard)) {
+                $("#ulModal").append(
+                    `<li><img src="
+                ${lastCard.image.small}" alt="Coin Icon"> ${lastCard.name} <button id="btn${i}" type="button" class="del btn btn-primary">delete</button></li>`
+                );
+                cards.push(lastCard);
+            }
+        });
+        setTimeout(() => { save(); }, 500);
+
+    }
+
+    // cencel
+    function cencelCard(cenceledCards) {
+        cards = cenceledCards;
+        console.log(cards);
+    }
+
+    // save
+    function save() {
+        console.log(cards);
+        location.reload(true);
+        $("#close").trigger("click");
+    }
+
 
     // live reports
-    function liveReports() { }
+    function liveReports(cards) {
+        $.map(cards, function (card) {
+            const cardSymbol = card.symbol;
+            $.ajax({
+                url: `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${cardSymbol}&tsyms=USD`,
+                success: link => {
+                    $('#liveReports').click(function () {
+                        prograssBar();
+                        // setInterval(() => {
+                        console.log((Object.values(link)[0].USD));
+                        var options = {
+                            exportEnabled: true,
+                            animationEnabled: true,
+                            title: {
+                                text: "Units Sold VS Profit"
+                            },
+                            subtitles: [{
+                                text: "Click Legend to Hide or Unhide Data Series"
+                            }],
+                            axisX: {
+                                title: "States"
+                            },
+                            axisY: {
+                                title: "Units Sold",
+                                titleFontColor: "#4F81BC",
+                                lineColor: "#4F81BC",
+                                labelFontColor: "#4F81BC",
+                                tickColor: "#4F81BC"
+                            },
+                            axisY2: {
+                                title: "Profit in USD",
+                                titleFontColor: "#C0504E",
+                                lineColor: "#C0504E",
+                                labelFontColor: "#C0504E",
+                                tickColor: "#C0504E"
+                            },
+                            toolTip: {
+                                shared: true
+                            },
+                            legend: {
+                                cursor: "pointer",
+                                itemclick: toggleDataSeries
+                            },
+                            data: [{
+                                type: "spline",
+                                name: "Units Sold",
+                                showInLegend: true,
+                                xValueFormatString: "MMM YYYY",
+                                yValueFormatString: "#,##0 Units",
+                                dataPoints: [
+                                    { x: new Date(2016, 0, 1), y: 120 },
+                                    { x: new Date(2016, 1, 1), y: 135 },
+                                    { x: new Date(2016, 2, 1), y: 144 },
+                                    { x: new Date(2016, 3, 1), y: 103 },
+                                    { x: new Date(2016, 4, 1), y: 93 },
+                                    { x: new Date(2016, 5, 1), y: 129 },
+                                    { x: new Date(2016, 6, 1), y: 143 },
+                                    { x: new Date(2016, 7, 1), y: 156 },
+                                    { x: new Date(2016, 8, 1), y: 122 },
+                                    { x: new Date(2016, 9, 1), y: 106 },
+                                    { x: new Date(2016, 10, 1), y: 137 },
+                                    { x: new Date(2016, 11, 1), y: 142 }
+                                ]
+                            },
+                            {
+                                type: "spline",
+                                name: "Profit",
+                                axisYType: "secondary",
+                                showInLegend: true,
+                                xValueFormatString: "MMM YYYY",
+                                yValueFormatString: "$#,##0.#",
+                                dataPoints: [
+                                    { x: new Date(2016, 0, 1), y: 19034.5 },
+                                    { x: new Date(2016, 1, 1), y: 20015 },
+                                    { x: new Date(2016, 2, 1), y: 27342 },
+                                    { x: new Date(2016, 3, 1), y: 20088 },
+                                    { x: new Date(2016, 4, 1), y: 20234 },
+                                    { x: new Date(2016, 5, 1), y: 29034 },
+                                    { x: new Date(2016, 6, 1), y: 30487 },
+                                    { x: new Date(2016, 7, 1), y: 32523 },
+                                    { x: new Date(2016, 8, 1), y: 20234 },
+                                    { x: new Date(2016, 9, 1), y: 27234 },
+                                    { x: new Date(2016, 10, 1), y: 33548 },
+                                    { x: new Date(2016, 11, 1), y: 32534 }
+                                ]
+                            }]
+                        };
+                        $("#chartContainer").CanvasJSChart(options);
+
+                        function toggleDataSeries(e) {
+                            if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                                e.dataSeries.visible = false;
+                            } else {
+                                e.dataSeries.visible = true;
+                            }
+                            e.chart.render();
+                        }
+                        // $('#cards').html();
+                        // }, 2000);
+                    });
+                },
+                error: () => console.error('API not valid')
+            });
+        });
+    }
 
 });
 
